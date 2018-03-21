6a2cec929d87e5e97d8b198a0897873d5c7697c8
Anh Quoc Tran <quoc.trananh@students.hueic.edu.vn>
2018-03-21 22:41:08 +0700

    Removed express view engine and static folder

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.listen(port, function () {
     console.log("Application is running at port: " + port)
 })
 
-app.set("view engine", "ejs")
-app.use('/public', express.static(__dirname + '/public'))
+// app.set("view engine", "ejs")
+// app.use('/public', express.static(__dirname + '/public'))
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
