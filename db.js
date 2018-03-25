f779ae15401a7ccf4c79a4fc5836fecba23eec97
Anh Quoc Tran <quoc.trananh@students.hueic.edu.vn>
2018-03-25 21:29:37 +0700

    fixed linter

--- db.js
+++ db.js
@@ -1,28 +1,27 @@
 var Sequelize = require('sequelize')
-var config = require('./config.json')
+//var config = require('./config.json')
 var winston = require('winston')
-const Op = Sequelize.Op;
 const operatorsAliases = require('./operatorsAliases')
 
 require('mysql')
 require('mysql2')
 require('dotenv').config()
 
-var sequelize = new Sequelize(process.env.MYSQL_DATABASE || "camera_vp9", process.env.MYSQL_USERNAME || "root", process.env.MYSQL_PASSWORD || 'anhquoc1996@@', {
-    host: process.env.MYSQL_SERVER || 'localhost',
-    port: process.env.MYSQL_PORT || 3306,
-    dialect: 'mysql',
-    operatorsAliases: operatorsAliases,
-    logging: winston.verbose
+var sequelize = new Sequelize(process.env.MYSQL_DATABASE || 'camera_vp9', process.env.MYSQL_USERNAME || 'root', process.env.MYSQL_PASSWORD || 'anhquoc1996@@', {
+	host: process.env.MYSQL_SERVER || 'localhost',
+	port: process.env.MYSQL_PORT || 3306,
+	dialect: 'mysql',
+	operatorsAliases: operatorsAliases,
+	logging: winston.verbose
 })
 
 sequelize.authenticate().then(function (err) {
-    if (err) {
-        console.error('Database connection error! ' + err);
-    } else {
-        console.log('Connection has been established successfully');
-    }
-});
+	if (err) {
+		console.error('Database connection error! ' + err)
+	} else {
+		console.log('Connection has been established successfully')
+	}
+})
 
 //Function class for handling database connection
 module.exports = sequelize
--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-var express = require("express")
-var bodyparser = require("body-parser")
+var express = require('express')
+var bodyparser = require('body-parser')
 var passport = require('passport')
 var morgan = require('morgan')
 
 require('dotenv').config()
-var morgan = require('morgan')
 
 const PORT = process.env.PORT || 1899
 var app = express()
 
 //config express middleware
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(function(req, res, next) {
-    res.removeHeader("X-Powered-By")
-    next();
+	res.removeHeader('X-Powered-By')
+	next()
 })
 
 app.use(morgan('dev'))
 require('./controllers/routes')(app, passport)
 app.listen(PORT, function () {
-    console.log("Application is running at port: " + PORT)
+	console.log('Application is running at port: ' + PORT)
 })
